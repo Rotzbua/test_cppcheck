e29c207021c864f1924d0a5b24ecf6d31596ff05
Rotzbua <Rotzbua@users.noreply.github.com>
2018-08-25 18:23:43 +0200

    Update cppcheck.sh

--- cppcheck.sh
+++ cppcheck.sh
@@ -19,7 +19,7 @@ if [ ! -f "${CACHED}/${NAMEVER}/cppcheck" ]; then
     cd ${QAWORKDIR}
     mkdir -p depbuild
     cd depbuild
-    curl -OL "http://downloads.sourceforge.net/project/cppcheck/cppcheck/${VERSION}/${NAMEVER}.tar.bz2"
+    curl -OL "https://downloads.sourceforge.net/project/cppcheck/cppcheck/${VERSION}/${NAMEVER}.tar.bz2"
     tar -xjf ${NAMEVER}.tar.bz2
     cd ${NAMEVER}
     echo "Actual build and install. This may take a while."
